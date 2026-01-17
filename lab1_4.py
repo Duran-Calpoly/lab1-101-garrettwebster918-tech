1634b88930429144ae6f07ab8387d39425687712
Garrett Webster <garrettwebster918@gmail.com>
2026-01-17 00:56:48 +0000

    done

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,13 @@
-pass
+def calculate_average(x, y, z):
+    return (x + y + z) / 3
+
+def add_tax(bill_total):
+    return bill_total * 1.10
+
+def greet_user():
+    name = input("What's your name? ")
+    print(f"Hello {name}!")
+
+print(add_tax(3))
+print(calculate_average(2,2,3))
+greet_user()
--- lab1_5.py
+++ lab1_5.py
@@ -1,8 +1,24 @@
-n = input("Enter number")
-check_multiple(n%3 == 0 and n%5 == 0)
-  return true
+n = int(input("Enter number: "))
 
-if check_multiple:
-  print("It is a multiple of 3 and 5")
+if(n%3 == 0 and n%5 == 0):
+    print("It is a multiple")
 else:
-  print("not a multiple")
+    print("not a multiple")
+
+
+n = input("Enter password")
+password = "abc123"
+if(n == password):
+    print("access granted")
+else:
+    print("access denied")
+
+def calculate_federal_tax(salary):
+    if salary <= 11000:
+        return salary * 0.10
+    elif salary <= 44725:
+        return salary * 0.12
+    elif salary <= 95375:
+        return salary * 0.22
+    else:
+        return salary * 0.24
